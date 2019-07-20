0bc15694e1dcc968542d5024ff3dcf2b6d0201db
Marcelo Paes Rocha <marcelo.rocha@nextsolucoes.net.br>
2019-07-20 17:33:44 -0400

    aula ate a 23

---
+++ video_10/main.py
@@ -0,0 +1,32 @@
+#-*- coding: UTF-8 -*-
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class Gerenciador(ScreenManager):
+    pass
+
+class Menu(Screen):
+    pass
+
+class Tarefas(Screen):
+    def __init__(self,tarefas=[],**kwargs):
+        super().__init__(**kwargs)
+        for tarefa in tarefas:
+            self.ids.box.add_widget(Tarefa(text=tarefa))
+
+    def addWidget(self):
+        texto = self.ids.texto.text
+        self.ids.box.add_widget(Tarefa(text=texto))
+        self.ids.texto.text = ''
+
+class Tarefa(BoxLayout):
+    def __init__(self,text='',**kwargs):
+        super().__init__(**kwargs)
+        self.ids.label.text = text
+
+class Test(App):
+    def build(self):
+        return Gerenciador()  #Tarefas(['Fazer compras','Buscar filho','Molhar a calçada','iu','iuhy','iug'])
+
+Test().run()
